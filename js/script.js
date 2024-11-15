0875da4a4526286dcf01ac9e5d38c2f0506a0ea3
Adil Zhexenov <tekmyktyadamdar@gmail.com>
2024-11-15 22:38:49 +0500

    lesson 2: variables

--- js/script.js
+++ js/script.js
@@ -1,2 +1,20 @@
-const titleBlock = document.getElementById("title");
-console.log(titleBlock);
+// String
+// Явное пр. - String(), num.toString()
+// Неявное пр. - "" + num
+
+// Number
+// Явное пр. - Number()
+// Неявное пр. - +str
+
+// Boolean
+// Явное пр. - Boolean()
+// Неявное пр. - !!variable
+// Когда false - '', 0
+// Когда true - все остальное
+
+
+const fileName = "data.csv";
+const fileSize = 2048;
+const message = `Файл ${fileName} успешно загружен. 
+Размер файла ${fileSize} КВ.`;
+console.log(message); 
