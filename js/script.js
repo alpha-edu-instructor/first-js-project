269e5e9207587de81567ba2f957bdffa742c3d92
Adil Zhexenov <tekmyktyadamdar@gmail.com>
2024-11-29 21:58:30 +0500

    lesson 8: functions

--- js/script.js
+++ js/script.js
@@ -1,39 +1,94 @@
-// While
-let balance = 100;
+// 1st task
+function add(a, b) {
+  return a + b;
+}
 
-while (balance > 0) {
-  const input = +prompt(`Остаток: ${balance}. Введите любое число:`);
+console.log(add(5, 9));
+console.log(add(41, 17));
+console.log(add(55, 21));
 
-  if (isNaN(input)) {
-    alert("Вы ввели некорректное значение!");
-    continue;
+// 2nd task
+function convertToCelcius(f) {
+  return ((f - 32) * 5/9).toFixed(2);
+}
+
+console.log(convertToCelcius(240));
+
+// 3rd task
+function compareAges(age1, age2) {
+  if (age1 > age2) {
+    return "Первый человек старше!";
+  } else if (age1 < age2) {
+    return "Второй человек старше!";
+  } else {
+    return "Оба человека одного возраста!";
+  }
+}
+
+console.log(compareAges(25, 30))
+console.log(compareAges(60, 50))
+console.log(compareAges(45, 45))
+
+// 4th task
+function sumArray(arr) {
+  let sum = 0;
+
+  for (const num of arr) {
+    sum += num;
   }
 
-  balance -= input;
-  console.log(`Остаток баланса: ${balance}`);
+  return sum;
 }
 
-alert("Ваш баланс отрицательный. Игра окончена!");
+console.log(sumArray([9, 5, 7, 25, 14]));
+console.log(sumArray([10, 20, 30, 40, 50, 60]));
 
-// For In
-const grades = {
-  math: 5,
-  physics: 4,
-  history: 3,
-  english: 4
-};
+// 5th task
+function findMax(arr) {
+  let max = arr[0];
 
-for (const key in grades) {
-  console.log(`${key}: ${grades[key]} `);
+  for (const num of arr) {
+    if (num > max) {
+      max = num;
+    }
+  }
+
+  return max;
 }
 
-// For Of
-const contries = ["Kazakhstan", "Great Britain", "France", "Italy"];
+console.log("findMax: " + findMax([9, 5, 39, 7, 25, 14]));
+console.log("findMax: " + findMax([10000, 210, 1050, -4, 4000, 20, 14, 8000]));
 
-for (let i = 0; i < contries.length; i++) {
-  console.log(`${i+1}. ${contries[i]}`);
+function findMin(arr) {
+  let min = arr[0];
+
+  for (const num of arr) {
+    if (num < min) {
+      min = num;
+    }
+  }
+
+  return min;
 }
 
-for (const country of contries) {
-  console.log(country);
+console.log("findMin: " + findMin([9, 5, 39, 7, 25, 14]));
+console.log("findMin: " + findMin([10000, 210, 1050, -4, 4000, 20, 14, 8000]));
+
+// 6th task
+function countEvenAndOdd(arr) {
+  const obj = {even: 0, odd: 0};
+
+  for (let i = 0; i < arr.length; i++) {
+    const num = arr[i];
+    
+    if (num % 2 === 0) {
+      obj.even += num
+    } else {
+      obj.odd += num;
+    }
+  }
+
+  return obj;
 }
+
+console.log(countEvenAndOdd([9, 5, 38, 7, 25, 14]));
